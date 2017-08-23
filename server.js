a754fdb4e4bd5b0403e7bac377d8e348734c4fb0
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-23 10:18:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/articleName' , function (req,res) {
+app.get('/:articleName' , function (req,res) {
     // articleName == Article-One
     // articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
