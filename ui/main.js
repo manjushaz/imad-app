901195c5f95da993f69d639b1266be205d6a4c9a
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 12:32:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,34 +30,3 @@ button.onclick = function() {
    request.send(null);
 };
 
-//var button = document.getElementById('counter');
-
-//button.onclick = function() {
-    
- //   console.log("things went perfect2");
-
-    // Create a request object
- //   var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in avariable
- //   request.onreadystatechange = function() {
-   //     console.log("things went perfect3");
-     //   if (request.readystate === XMLHttpRequest.DONE ) {
-            // Take some action.
-       //     if (request.status === 200) {
-         //       var counter = request.responseText;
-           //     var span = document.getElementById('count');
-             //   span.innerHTML = counter.toString();
-              //  console.log("things went perfect4");
-            //    console.log(span.innerHTML);
-            //}
-        //}
-        // Not done yet
-        
-    //};
-    
-    // Make the request
-    
-    //request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/counter', true);
-    //request.send(null);
-//};
