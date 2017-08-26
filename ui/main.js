3d15a21e9ab6599a4032379a5eac1a9afd6a1d86
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 12:31:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,33 @@
- console.log("things went perfect1");
+console.log("things went perfect1");
+ 
+// Counter code
 
 var button = document.getElementById('counter');
- var counter = 0;
+
 button.onclick = function() {
-    console.log('counter incremented');
-   
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    console.log('request created');
     
+   // create a request object
+   var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function () {
+       
+       // Capture the request and store it in a variable
+       
+       if ( request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+           }
+       }
+       // Not done yet
+   };
+   
+   // Make the request
+   request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
 
 //var button = document.getElementById('counter');
