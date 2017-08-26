c4f4003721f7e007cb29531624c53b4db0c169ab
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 13:04:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ submit.obclick = function() {
     
     // Capture a list of names and render as a list
     
-    var names=['name1','name2','name3'];
+    var names=['name1','name2','name3','name4'];
     var list = '';
     for (var i=0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>'; 
