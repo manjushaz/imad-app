97d2f5052a36dead17d7435a66a6bb8840a3a4e9
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 10:59:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,10 @@ app.get('/', function (req, res) {
 });
 
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -110,9 +114,6 @@ app.get('/:articleName' , function (req,res) {
 });
 
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
