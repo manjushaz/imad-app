be40c9c80178445d63ca4e466ceb7fdead27c77c
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 11:32:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,41 @@
  console.log("things went perfect1");
 // Counter code
 
-var button = document.getElementById('counter');
-
-button.onclick = function() {
+butoon.onclick = function() {
     
-    console.log("things went perfect2");
+    var counter = 0;
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+//var button = document.getElementById('counter');
+
+//button.onclick = function() {
+    
+ //   console.log("things went perfect2");
 
     // Create a request object
-    var request = new XMLHttpRequest();
+ //   var request = new XMLHttpRequest();
     
     // Capture the response and store it in avariable
-    request.onreadystatechange = function() {
-        console.log("things went perfect3");
-        if (request.readystate === XMLHttpRequest.DONE ) {
+ //   request.onreadystatechange = function() {
+   //     console.log("things went perfect3");
+     //   if (request.readystate === XMLHttpRequest.DONE ) {
             // Take some action.
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                console.log("things went perfect4");
-                console.log(span.innerHTML);
-            }
-        }
+       //     if (request.status === 200) {
+         //       var counter = request.responseText;
+           //     var span = document.getElementById('count');
+             //   span.innerHTML = counter.toString();
+              //  console.log("things went perfect4");
+            //    console.log(span.innerHTML);
+            //}
+        //}
         // Not done yet
         
-    };
+    //};
     
     // Make the request
     
-    request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+    //request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/counter', true);
+    //request.send(null);
+//};
