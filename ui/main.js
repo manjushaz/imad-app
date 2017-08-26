9490f5dcd38ab8bec3afa69ccc2251e8a97e81b7
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 10:55:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick = function() {
             // Take some action.
             if (request.status === 200) {
                 var counter = request.responseText;
-                var span = document.geElementById('count');
+                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
                 console.log("things went perfect");
                 console.log(span.innerHTML);
