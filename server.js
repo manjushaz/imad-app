443c782fea4a9b803e3b1dd766e29e6a2e26331a
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-26 10:59:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,11 @@ app.get('/favicon.ico', function (req, res) {
 
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 var counter = 0;
 app.get('/counter', function ( req, res) {
     counter = counter + 1;
@@ -97,10 +102,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
