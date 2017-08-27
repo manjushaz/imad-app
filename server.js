2de02794a6873aa7ef6b41b94d05c6f85ef9c19d
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-27 18:31:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/submit-name', function (req, res) { // URL:/submit-name?name=xxxxx
 app.get('/articles/:articleName' , function (req,res) {
     // articleName == Article-One
     // articles[articleName] == {} content object for article one
-    pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err, result) {
+    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'", function(err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
