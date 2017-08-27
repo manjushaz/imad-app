135d1ebb8118a2102fe353348606d83a1daccd86
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-27 18:33:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ function createTemplate (data) {
                 <h3> ${heading} 
                 </h3>
                 <div>
-                    ${date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                     ${content}            
