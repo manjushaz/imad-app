ab7238973fbfb9c71eaf62aa3985b74603461da6
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-29 16:05:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 function hash(input, salt) {
     // How to create a hashed string
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
     
 }
 
