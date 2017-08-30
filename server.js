1c35c6f7694c142fcfda47760650f4769834acfc
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-30 12:53:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,20 @@ app.post('/create-user', function (req, res) {
 });
 
 
+app.post('/login', function (req, res) {
+
+   var username = req.body.username;
+   var password = req.body.password;
+
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
 app.post('/login', function(req , res) {
 
     var username = req.body.username;
