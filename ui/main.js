47e6fa5433c2aa9e4faa20077b9625c05083bccd
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-30 13:49:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,6 @@
-console.log("things went perfect1");
- 
-// Counter code
+// Submit username and password to login
 
-var button = document.getElementById('counter');
-
-button.onclick = function() {
-    console.log('request created');
-    
-   // create a request object
-   var request = new XMLHttpRequest();
-   
-   request.onreadystatechange = function () {
-       
-       // Capture the request and store it in a variable
-       
-       if ( request.readyState === XMLHttpRequest.DONE) {
-           // Take some action
-           if (request.status === 200) {
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-               span.innerHTML = counter.toString();
-           }
-       }
-       // Not done yet
-   };
-   
-   // Make the request
-   request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
-
-
-// Submit name
-
-var submit = document.getElementById('submitBtn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
    // create a request object
@@ -46,23 +13,25 @@ submit.onclick = function() {
        if ( request.readyState === XMLHttpRequest.DONE) {
            // Take some action
            if (request.status === 200) {
-                var names=request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i<names.length; i++) {
-                    list += '<li>' + names[i] + '</li>'; 
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-           }
+               console.log('user loggedin');
+               alert('Logged in successfully');
+            } else if (request.status ===403) {
+                    alert('Username/password is incorrect.');
+            } else if (request.staus === 500) {
+                        alert('Something went wrong on the server.');
+            }
        }
        // Not done yet
    };
    
    // Make the request
-   var nameInput = document.getElementById('name');
-   var nameValue = nameInput.value;
-   request.open('GET', 'http://manjushaz2012.imad.hasura-app.io/submit-name?name=' + nameValue, true);
-   request.send(null);
+   
+   var username = document.getElementById('username');
+   var password = document.getElementById('password');
+   console.log(username);
+   console.log(password);
+   request.open('POST', 'http://manjushaz2012.imad.hasura-app.io/login', true);
+   request.setRequestHeader('Conternt-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password}));
     
 };
