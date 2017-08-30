7acea6a094adeac295dabe16637a8e9d5449d5cf
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-30 13:55:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,12 +26,12 @@ submit.onclick = function() {
    
    // Make the request
    
-   var username = document.getElementById('username');
-   var password = document.getElementById('password');
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value;
    console.log(username);
    console.log(password);
    request.open('POST', 'http://manjushaz2012.imad.hasura-app.io/login', true);
-   request.setRequestHeader('Conternt-Type', 'application/json');
-   request.send(JSON.stringify({username: username, password: password}));
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password})); 
     
 };
