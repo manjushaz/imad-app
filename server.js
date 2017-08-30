2df1516e28bda6c6010bd6d2c57445158b476377
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-08-30 13:02:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,21 @@ app.post('/login', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send('User successfully created: ' + username);
+          if (result.rows.length === 0) {
+            res.status(403).send('username/password is invalid');             
+          } else {
+              //Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                  res.send('credentials correct!');
+              } else {
+                  res.status(403).send('username/password is invalid');
+              }
+              
+          }
+          
       }
    });
 });
