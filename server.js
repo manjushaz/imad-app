65d90b6f0b4dbd319bf71b96b8033f987f0a9184
manjushaz <30747524+manjushaz@users.noreply.github.com>
2017-09-06 11:22:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
+app.post('/create_user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
